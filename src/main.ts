af03e8782f92329be7d406c203bba038323ce5af
jallouli yassine <jungloul1@gmail.com>
2024-05-08 01:52:33 +0200

    railway

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     credentials: true, // Allow sending cookies from the frontend
   });
 
-  await app.listen(3000,"0.0.0.0");
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 
 bootstrap();
