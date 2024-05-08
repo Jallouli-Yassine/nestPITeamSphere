af69900bd0d2335a06c1dfcafc70238624758355
jallouli yassine <jungloul1@gmail.com>
2024-05-08 21:40:42 +0200

    bug

--- src/modules/user/socket.io.ts
+++ src/modules/user/socket.io.ts
@@ -30,6 +30,7 @@ export class WebsocketGateway {
 
   @SubscribeMessage("Connect")
   handleConnect(client: Socket, user: User): void {
+    console.log("dehgehehe : ",client);
     user.socketid = client.id;
     if(this.users){
       if(this.users.find(u => u.id == user.id)) {
